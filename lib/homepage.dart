9cfdaa3b49933e336add36bc4de34ab13b316b9f
Ishwor Panta <ishworpanta10@gmail.com>
2019-02-04 22:20:55 +0545

    app name changed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -270,3 +270,42 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
+//  Widget build1(BuildContext context) {
+//     return new SplashScreen(
+//       seconds: 14,
+//       navigateAfterSeconds: new AfterSplash(),
+//       title: new Text('Welcome In SplashScreen',
+//       style: new TextStyle(
+//         fontWeight: FontWeight.bold,
+//         fontSize: 20.0
+//       ),),
+//       image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+//       backgroundColor: Colors.white,
+//       styleTextUnderTheLoader: new TextStyle(),
+//       photoSize: 100.0,
+//       onClick: ()=>print("Flutter Egypt"),
+//       loaderColor: Colors.red
+//     );
+//   }
+// }
+
+// class AfterSplash extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//       title: new Text("Welcome In SplashScreen Package"),
+//       automaticallyImplyLeading: false
+//       ),
+//       body: new Center(
+//         child: new Text("Done!",
+//         style: new TextStyle(
+//           fontWeight: FontWeight.bold,
+//           fontSize: 30.0
+//         ),),
+
+//       ),
+//     );
+//   }
--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -9,16 +9,62 @@ class ProfilePage extends StatelessWidget {
         title: Text("User Profile"),
         
       ),
-      body: Container(
-        height: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(profile), 
-            fit: BoxFit.cover
-          )
-        ),
+      // body: Container(
+      //   height: double.infinity,
+      //   decoration: BoxDecoration(
+      //     image: DecorationImage(
+      //       image: AssetImage(profile), 
+      //       fit: BoxFit.cover
+      //     )
+      //   ),
        
-      ),
+      // ),
+    body: Stack(
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: 400,
+              decoration: BoxDecoration(
+                image:DecorationImage(
+                  image: AssetImage("assets/img11.jpg"), 
+                  fit: BoxFit.fill,
+                )
+              ),
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+
+            Container(
+              height: 413,
+              decoration: BoxDecoration(
+                color: Colors.greenAccent,
+              ),
+              child: Center(child: Text("Ishwor Panta",style: TextStyle(fontSize: 45),),
+              ),
+            )
+
+          ],
+        ),
+        Positioned(top: 150,
+        child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(
+              width: 4,
+              color: Colors.white,
+            ),
+            image: DecorationImage(image: AssetImage("assets/ishwor.jpg"))
+
+          ),
+        ),)
+      ],
+    ),
+
     );
   }
 }
